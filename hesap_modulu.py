9df0de13d2e1f5c92f7bdf456575210d94bd62b6
Ayşe Nur Türkaslan <63192605+nurrturkaslan@users.noreply.github.com>
2021-08-07 10:28:54 +0300

    Add files via upload

---
+++ hesap_modulu.py
@@ -0,0 +1,8 @@
+
+
+def yeni_maas(x):
+    print(x*20/100+x)
+    
+    
+    
+maaslar = [1000,2000,3000,4000]
---
+++ untitled2.py
@@ -0,0 +1,18 @@
+import hesap_modulu
+
+hesap_modulu.yeni_maas(1000)
+
+#vs
+
+import hesap_modulu as hm
+hm.yeni_maas(2000)
+
+#vs
+
+from hesap_modulu import yeni_maas
+yeni_maas(3000)
+
+
+import hesap_modulu as hm
+hm.maaslar
+
